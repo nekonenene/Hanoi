fec472351416af8f8254039eda44e8c173e0b650
nekonenene <hato.nekoe@gmail.com>
2017-03-27 21:02:40 +0900

    解法例を書き入れた

--- src/main/kotlin/com/x0/hatonekoe/Main.kt
+++ src/main/kotlin/com/x0/hatonekoe/Main.kt
@@ -1,5 +1,14 @@
 package com.x0.hatonekoe
 
 fun main(args: Array<String>) {
-    println("Hello!!")
+    moveDisc(4, 'A', 'B', 'C')
+}
+
+/* http://www13.plala.or.jp/kymats/study/C++/Hanoi/Hanoi.html より */
+fun moveDisc(discNumber: Int, fromPole: Char, workingPole: Char, toPole: Char) {
+    if (discNumber > 0) {
+        moveDisc(discNumber - 1, fromPole, toPole, workingPole)
+        println(discNumber.toString() + "を" + fromPole + "から" + toPole + "へ")
+        moveDisc(discNumber - 1, workingPole, fromPole, toPole)
+    }
 }
